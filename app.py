a625e830c956c892c601e020f5f598a68980a71a
prince3453 <princeghoghari9@gmail.com>
2024-02-06 05:29:29 -0500

    added the routing and variable rule

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect, url_for, jsonify
 
 # entry point of the application
 app = Flask(__name__)
@@ -13,9 +13,41 @@ def index():
     return "<h2>Welcome to Index Page</h2>"
 
 # variable rules
-@app.route('/sucess/<score>', methods=['GET'])
+@app.route('/sucess/<int:score>', methods=['GET'])
 def sucess(score):
-    return "<h2>The person has passed the exam with the score of </h2>" + score
+    return "The person has passed the exam with the score of " + str(score)
+
+@app.route('/failure/<int:score>', methods=['GET'])
+def failure(score):
+    return "The person has failed the exam with the score of " + str(score)
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+        # for the render template we need to have the form.html in the templates folder
+        return render_template('form.html')
+    else:
+        maths = int(request.form['maths'])
+        chemistry = int(request.form['chemistry'])
+        physics = int(request.form['physics'])
+
+        avg_marks = (maths + chemistry + physics)/3
+
+        result = ""
+
+        if avg_marks > 50:
+            result = "sucess"
+        else:
+            result ="failure"     
+        return redirect(url_for(result, score=avg_marks))      
+       # return render_template('form.html', Score=avg_marks)  
+    
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    a_val = float(dict(data)['a'])
+    b_val = float(dict(data)['b'])
+    return jsonify({'result': a_val+b_val})
 
 # entry point denotes using the __
 if __name__ == '__main__':
